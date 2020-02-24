b55bb3253a898943cbf1d556c36cb5d4866ba424
Jamie Davis <davisjam@vt.edu>
2020-02-24 17:47:22 -0500

    more test cases

--- memo-test/test.pl
+++ memo-test/test.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/env perl
 
-use re 'debug';
+#use re 'debug';
 use strict;
 use warnings;
 
@@ -13,7 +13,7 @@ use warnings;
 
 
 #if ("a" x 500 . "!" =~ m/<a.*href="(.*)".*>(.*)<\/a>/) {
-my $input = "'" . "0" x 100 . ".";
+my $input = "'" . "0" x 1000 . ".";
 print "$input\n";
 #if ($input =~ m/\d\d*\d*\d*\d*\d*\d*$/) { # Not protected on 0000 -- Only STAR, no CURLYX
 #if ($input =~ m/\d\d+\d+\d+\d+\d+\d+$/) { # Not protected on 0000 -- Only PLUS, no CURLYX
@@ -22,8 +22,13 @@ print "$input\n";
 #if ($input =~ m/(\d+)+$/) { # Protected by SL cache on 0000.
 #if ($input =~ m/([\d]+)*$/) { # Protected by SL cache on 0000.
 #if ($input =~ m/(\d+)*\1$/) { # Cache cannot help; we wipe it whenever we test the backref
-if ($input =~ m/('|")(\d+)*\1$/) { # Cache cannot help; we wipe it whenever we test the backref
+#if ($input =~ m/('|")(\d+)*\1$/) { # Cache cannot help; we wipe it whenever we test the backref
 #if ($input =~ m/\d(\d?)*(\d?)*(\d?)*(\d?)*(\d?)*(\d?)*$/) { # Protected by SL cache on 0000. -- For A* = (\d?)* , the A is not fixed-width, so Perl expands this into a CURLYX{0,INFTY}. It caches those!
+#if ($input =~ m/(\d{1,500}){1,500}$/) { # Not protected, because not a CURLYX -- bounded quantifiers
+if ($input =~ m/(\d*){1,100}$/) { # Not protected, because not a CURLYX -- bounded quantifiers
+#if ($input =~ m/(\d|\d)+$/) { # Not protected, because not a CURLYX -- fixed-width A
+#if ($input =~ m/(\d*)+(\d*)+(\d*)+(\d*)+(\d*)+(\d*)+(\d*)+(\d*)+(\d*)+(\d*)+(\d*)+(\d*)+(\d*)+(\d*)+(\d*)+$/) { # Protected -- 16 CURLYX
+#if ($input =~ m/(\d?)+(\d?)+(\d?)+(\d?)+(\d?)+(\d?)+(\d?)+(\d?)+(\d?)+(\d?)+(\d?)+(\d?)+(\d?)+(\d?)+(\d?)+(\d*)+$/) { # Not protected -- 17 CURLYX
   print "Match\n";
 } else {
   print "No match\n";
